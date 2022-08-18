8b7338617b466a52ead57d05e5d92a323b0fffe5
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-18 13:37:28 +0300

    create homepage and room path

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,14 @@
+from urllib import request, response
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    
+    
+    return render(request, 'mainapp/home.html')
+
+def room(request):
+    
+    
+    return render(request, 'mainapp/room.html')
