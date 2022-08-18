f9fade038c49f966d782dc8655e55024e69573a0
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-18 15:51:15 +0300

    create a dummy dictionary and iterate over them in the template

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {"id":1, "name": "Nthe first"},
+    {"id":2, "name": "Second"},
+    {"id":3, "name": "Third one"}
+]
 
 def home(request):
+    context = {
+        "rooms":rooms
+    }
     
-    
-    return render(request, 'mainapp/home.html')
+    return render(request, 'mainapp/home.html', context)
 
-def room(request):
+def room(request, pk):
     
-    
+  
     return render(request, 'mainapp/room.html')
