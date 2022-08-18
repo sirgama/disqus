fddfe78085b293588479e4b1d1e9a4ed2795c2b5
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-18 14:30:51 +0300

    setup solar navbar

--- disqus/settings.py
+++ disqus/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-vr$(4997)#h(gqac(9t5+_5090-!6=zhl%sv5028+m^f3ba86h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
