ca1a7612d2d3e07c547b05830c7a1f391c638ec4
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-19 05:35:36 +0300

    add a class meta function to order items by most recent item first

--- mainapp/models.py
+++ mainapp/models.py
@@ -14,11 +14,16 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-updated', '-created']
     def __str__(self):
         return self.name
+    
+    
     
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
