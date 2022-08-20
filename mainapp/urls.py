9cdbf8bb27816ff30cf0a898c1e6b6b52d24aee5
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-20 23:06:22 +0300

    add delete txt link and url path

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('new/', views.newroom, name= 'newroom'),
     path('update/<str:pk>/', views.updateRoom, name='update'),
     path('delete/<str:pk>/', views.delete, name='delete'),
+    path('deletemsg/<str:pk>/', views.deleteMessage, name='delete-txt'),
     path('logout/', views.logoutUser, name='logout')
 ]
