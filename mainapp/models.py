2f2f84c3259d61662b9e177eae5fa09b45da4aec
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-20 21:41:27 +0300

    add members to room model

--- mainapp/models.py
+++ mainapp/models.py
@@ -14,7 +14,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    #participants = 
+    members = models.ManyToManyField(User, related_name='members', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
